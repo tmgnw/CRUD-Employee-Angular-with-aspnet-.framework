596a875ba06de984c8769963e2387a024cae1e7c
tmgnw <61616667+tmgnw@users.noreply.github.com>
2020-06-04 13:40:42 +0700

    Create Form, Configure Api, Insert Data

---
+++ Project/Angular7/src/app/shared/employee.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  formData : Employee;
+
+  readonly rootURL ="https://localhost:44311/api"
+
+  constructor(private http : HttpClient) { }
+  
+  postEmployee(formData : Employee){
+    return this.http.post(this.rootURL+'/Employee',formData);
+     
+   }
+}
